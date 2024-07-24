b4bd479db2b65721475dc9afebad7d3f7347f269
MINEDUCYT <7178497@clases.edu.sv>
2024-07-24 11:21:47 -0600

    se creo la repo

---
+++ src/main/java/com/example/DEML24072024/Deml24072024Application.java
@@ -0,0 +1,13 @@
+package com.example.DEML24072024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deml24072024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deml24072024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DEML24072024/Deml24072024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DEML24072024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deml24072024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
